cd5a19f527331ac2d7b6f5e33937786d2e6b6c84
cesartorress-008 <cesartorres17072013@gmail.com>
2023-11-06 16:01:38 -0300

    nueva prueba de compilacion

--- WebNotas/Models/Nota.cs
+++ WebNotas/Models/Nota.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Titulo { get; set; } = string.Empty;
         public string Contenido { get; set; } = string.Empty;
+
         public Byte[]? Imagen { get; set; }
     }
 }
